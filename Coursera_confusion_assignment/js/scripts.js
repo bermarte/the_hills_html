1e8d0b647bfa75968c09185677130a767c2acc85
heidi <bermarte@hotmail.com>
2020-04-14 12:42:35 +0200

    complete sass scss file

--- Coursera_confusion_assignment/js/scripts.js
+++ Coursera_confusion_assignment/js/scripts.js
@@ -8,11 +8,17 @@ $(document).ready(function() {
       $("#mycarousel").carousel('pause');
       $("#carouselButton").children('span').removeClass('fa-pause');
       $("#carouselButton").children('span').addClass('fa-play');
-    }
-    else if ($("#carouselButton").children('span').hasClass('fa-play')) {
+    } else if ($("#carouselButton").children('span').hasClass('fa-play')) {
       $("#mycarousel").carousel('cycle');
       $("#carouselButton").children('span').removeClass('fa-play');
       $("#carouselButton").children('span').addClass('fa-pause');
     }
+  });
+
+  $("#reserveTable").click(function() {
+    $("#modalReserve").modal();
+  });
+  $("#login").click(function() {
+    $("#loginModal").modal();
   });
 });
